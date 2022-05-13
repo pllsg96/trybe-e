d8c1eb920e2a8ed055862f96bcf581369c24ebf5
Pedro Luiz <pedroluizlsg@gmail.com>
2022-05-13 15:16:12 -0300

    Exercicios dia 1

---
+++ fundamentos/intro-js/array-and-loop/array/script.js
@@ -0,0 +1,18 @@
+  // let pizza1 = "4 Queijos";
+  // let pizza2 = "Marguerita";
+  // let pizza3 = "Frango com catupiry";
+  // let pizza4 = "Calabresa";
+  // let pizza5 = "Verona";
+
+let pizzas = ["4quejos", "Marguerita", "Frango com catupiry", "Palmito", "Calabresa"];
+pizzas.push('Peito de peru');
+pizzas.unshift("Bacon")
+// console.log(pizzas);
+// console.log(pizzas.sort());
+// console.log(pizzas.length);
+// console.log(pizzas[2]);
+
+
+for (let index = 0; index < pizzas.length; index++){
+  console.log(pizzas[index]);
+}
