d3c5d9751b9e7d5df528c0f8f3dfe1cc5348d13a
Pedro Luiz <pedroluizlsg@gmail.com>
2022-05-19 18:29:14 -0300

    Adiciona seletores de elementos

--- fundamentos/dom-e-seletores-js/dom-and-selectors/seletores-de-elementos/querySelector/script.js
+++ fundamentos/dom-e-seletores-js/dom-and-selectors/seletores-de-elementos/querySelector/script.js
@@ -9,3 +9,19 @@ console.log(elementosSpan);
 elementosSpan[0].innerText = "Carlos Sainz";
 
 console.log(elementosSpan);
+
+// console.log(document.querySelector(".pilotos-sem-titulo p"));
+
+// console.log(document.querySelector(".pilotos-sem-titulo .piloto-f1-atual"));
+
+//document.querrySelector("#Dona") para selecionar ID
+//document.querrySelector("p"); Retorna apenas o primeiro elemento (o primeiro que ele encontra, independente de ser uma classe).
+
+//document.querrySelector(".pilotos-sem-titulo p"), faz uma seleção de um item limitando o seletor de forma similar ao css
+
+//document.querrySelector(".piloto-f1-atual") retorna o primeiro que tem essa classe dentre tds os elementos
+
+//document.querrySelector(".piloto-f1-atual .piloto-f1-atual") retorna o primeiro que tem essa classe dentre tds os elementos, levando em consideração todos os seletores desejados.
+
+//document.querrySelectorAll tem o comportamento semelhante ao querrySelector, porém ele retorna um array com todos os elementos que fazem parte de sua seleção, ao invés de retornar apenas o primeiro deles. Ele pode substituir o document.selectElementByClassName.
+
---
+++ fundamentos/dom-e-seletores-js/dom-and-selectors/seletores-de-elementos/querySelectorAll/script.js
@@ -0,0 +1,7 @@
+//o querrySelector irá retornar o primeiro item que ele achar de acordo com os parâmetros passados.
+
+//o querrySelectorAll irá retornar todos os itens que ele encontrar de acordo com os parâmetros passados, ele retorna no caso uma nodeList.
+//O nodeList é diferente do HTMLcollection que irá ter apenas elementos HTML. No nodeList ele poderá conter um texto que não necessáriamente é um elemento HTML. 
+//Para alterar algo no nodeList, irá ser necessário acessar primeiramente a posição do elemento que se deseja alterar, para assim colocar o parâmetro que se deseja alterar.
+//Assim como o HTML collection o node List terá seu length (comprimeno), afinal ambos são similares a arrays.
+//
