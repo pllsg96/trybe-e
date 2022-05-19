5f5796ffa70c2789d7711312e579ce43fcd8c199
Pedro Luiz <pedroluizlsg@gmail.com>
2022-05-19 19:54:54 -0300

    Adiciona exercicios e conteudo dom-e-seletores

---
+++ fundamentos/dom-e-seletores-js/dom-and-selectors/quando-usar/script.js
@@ -0,0 +1,2 @@
+// O getElementById(id) funciona de maneira diferente do querySelector('#id'), mas o resultado é o mesmo. Como dito no vídeo, o primeiro tem uma performance melhor, mas o segundo é mais flexível.
+// Enquanto os getElementsByClassName('ClassName') e getElementsByTagName('TagName') retornam uma HTMLCollection, os querySelectorAll('.ClassName') e querySelectorAll('TagName') retornam uma NodeList.
