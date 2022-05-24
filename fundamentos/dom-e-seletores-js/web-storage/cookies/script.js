11b55a8b708386b1e07c9a9255ae3878c47c02fd
Pedro Luiz <pedroluizlsg@gmail.com>
2022-05-24 20:01:49 -0300

    Exercicios e assunto do web storage

---
+++ fundamentos/dom-e-seletores-js/web-storage/cookies/script.js
@@ -0,0 +1,14 @@
+document.cookie = "email = pedroluizlsg@gmail.com; expires=Thu, 17 Dec 2022 12:00:00 UTC; path=/";
+
+var myCookie = document.cookie;
+console.log(myCookie);
+
+document.cookie = "email = tirei0@gmail.com; expires=Thu, 17 Dec 2022 12:00:00 UTC; path=/";
+
+myCookie = document.cookie;
+
+console.log(myCookie);
+
+document.cookie = "email = pedroluizlsg@gmail.com; expires=Thu, 17 Dec 2019 12:00:00 UTC; path=/";
+myCookie = document.cookie;
+console.log(myCookie);
