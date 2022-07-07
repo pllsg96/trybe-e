aa719435bd8ec821f4f65d017005defb9362efa6
Pedro Luiz <pedroluizlsg@gmail.com>
2022-07-07 18:01:08 -0300

    ADiciona bloco 9

---
+++ front-end/bloco9-js-e-testes-assincronos/js-assincrono-e-callback/p2-callbacks/exVideo.js
@@ -0,0 +1,12 @@
+// --- --- --- --- --- --- --- ---
+// Callback exemplo
+const resultadoFinal = (resultado) => {
+  console.log(resultado);
+}
+
+const funcSoma = (num1, num2, callback) => {
+  let soma = num1 + num2;
+  callback(soma);
+}
+
+funcSoma(10, 8, resultadoFinal);
--- front-end/bloco9-js-e-testes-assincronos/js-assincrono-fetch-api-async-await/p4-fetch-api/ex-video.js
+++ front-end/bloco9-js-e-testes-assincronos/js-assincrono-fetch-api-async-await/p4-fetch-api/ex-video.js
@@ -2,6 +2,8 @@
 const fetch = require('node-fetch');
 const getRandomAdvice = () => {
   const url = 'https://api.adviceslip.com/advice';
-  const request = fetch(url);
+  const request = fetch(url).then((response) => console.log(response.json()));
   console.log(request);
 };
+
+getRandomAdvice();
